f179fb7f932ac80066d0262a957182fd83ab55b5
Brey007 <sirope44@gmail.com>
2026-03-04 16:40:09 -0500

    Se configuro el entorno
    - Imagen de logo, componente navbar y app, index y main configurado

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import argoslogo from '../../assets/argoslogo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <div><a href="#">
+                <img src = {argoslogo} alt="Logo" />
+                </a></div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
